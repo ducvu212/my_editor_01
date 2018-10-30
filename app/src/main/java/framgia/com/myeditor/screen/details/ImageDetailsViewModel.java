711e5fbc26b539c199943fabac27c78a0968a715
DUC_PC <minhducvu.212@gmail.com>
2018-10-30 13:10:00 +0700

    Update Handle UI Details Screen

---
+++ app/src/main/java/framgia/com/myeditor/screen/details/ImageDetailsViewModel.java
@@ -0,0 +1,110 @@
+package framgia.com.myeditor.screen.details;
+
+import android.app.DownloadManager;
+import android.arch.lifecycle.Lifecycle;
+import android.arch.lifecycle.LifecycleOwner;
+import android.arch.lifecycle.LifecycleRegistry;
+import android.content.Context;
+import android.support.annotation.NonNull;
+import framgia.com.myeditor.data.model.ImageRandom;
+import framgia.com.myeditor.data.repository.ImageRepository;
+import framgia.com.myeditor.screen.base.BaseViewModel;
+import framgia.com.myeditor.utils.common.DisplayUtils;
+import framgia.com.myeditor.utils.rx.BaseSchedulerProvider;
+import io.reactivex.Observable;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.disposables.Disposable;
+
+import static framgia.com.myeditor.screen.home.adapter.RandomPagerAdapter.NUMBER_ONE;
+
+/**
+ * Exposes the data to be used in the ImageDetails screen.
+ */
+
+public class ImageDetailsViewModel extends BaseViewModel implements LifecycleOwner {
+
+    private Context mContext;
+    private BaseSchedulerProvider mSchedulerProvider;
+    private CompositeDisposable mCompositeDisposable = new CompositeDisposable();
+    private LifecycleRegistry mLifecycleRegistry;
+    private ImageRepository mRepository;
+    private ImageRandom mImageRandomDownload;
+    private ImageDetailsViewListener mListener;
+
+    ImageDetailsViewModel(Context context, ImageRepository repository,
+            ImageDetailsViewListener imageDetailsViewListener) {
+        mContext = context;
+        mRepository = repository;
+        mLifecycleRegistry = new LifecycleRegistry(this);
+        mLifecycleRegistry.markState(Lifecycle.State.CREATED);
+        mListener = imageDetailsViewListener;
+    }
+
+    @NonNull
+    @Override
+    public Lifecycle getLifecycle() {
+        return mLifecycleRegistry;
+    }
+
+    @Override
+    protected void onStart() {
+        mLifecycleRegistry.markState(Lifecycle.State.STARTED);
+    }
+
+    @Override
+    protected void onStop() {
+        mCompositeDisposable.clear();
+    }
+
+    void setSchedulerProvider(BaseSchedulerProvider schedulerProvider) {
+        mSchedulerProvider = schedulerProvider;
+    }
+
+    void updateImageLike(ImageRandom imageRandom) {
+        Disposable disposable = Observable.create(emitter -> {
+            mRepository.updateImage(imageRandom);
+            emitter.onComplete();
+        }).subscribeOn(mSchedulerProvider.io()).observeOn(mSchedulerProvider.ui()).subscribe(o -> {
+
+                }, throwable -> DisplayUtils.makeToast(mContext, throwable.getMessage()),
+                () -> mListener.updateLikeButton(imageRandom.getLikeByUser()));
+        mCompositeDisposable.add(disposable);
+    }
+
+    void updateDownload() {
+        Disposable disposable = Observable.create(emitter -> {
+            mRepository.updateDownload(mImageRandomDownload);
+            emitter.onComplete();
+        })
+                .subscribeOn(mSchedulerProvider.io())
+                .observeOn(mSchedulerProvider.ui())
+                .subscribe(o -> mImageRandomDownload.setDownloaded(NUMBER_ONE),
+                        throwable -> DisplayUtils.makeToast(mContext, throwable.getMessage()),
+                        () -> {
+                            mListener.updateDownloadButton(mImageRandomDownload.getDownloaded());
+                            mImageRandomDownload.setDownloaded(NUMBER_ONE);
+                        });
+        mCompositeDisposable.add(disposable);
+    }
+
+    void download(ImageRandom imageRandom) {
+        mImageRandomDownload = imageRandom;
+        mImageRandomDownload.setDownloaded(NUMBER_ONE);
+        mListener.updateDownloadButton(NUMBER_ONE);
+        mRepository.dowloadImage(
+                (DownloadManager) mContext.getSystemService(Context.DOWNLOAD_SERVICE),
+                imageRandom.getPath(), imageRandom.getImageId(), mListener);
+    }
+
+    void insertImage(ImageRandom imageRandom) {
+        Disposable disposable = Observable.create(emitter -> {
+            mRepository.insertImage(imageRandom);
+            emitter.onComplete();
+        }).subscribeOn(mSchedulerProvider.io()).observeOn(mSchedulerProvider.ui()).subscribe(o -> {
+
+        }, throwable -> DisplayUtils.makeToast(mContext, throwable.getMessage()), () -> {
+
+        });
+        mCompositeDisposable.add(disposable);
+    }
+}
