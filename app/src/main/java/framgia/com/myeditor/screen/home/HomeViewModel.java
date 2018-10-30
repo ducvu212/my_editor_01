e32f15917ab9a3b406ceba259fe2a8393effb508
DUC_PC <minhducvu.212@gmail.com>
2018-10-30 09:09:21 +0700

    Update Handle UI Home Screen

--- app/src/main/java/framgia/com/myeditor/screen/home/HomeViewModel.java
+++ app/src/main/java/framgia/com/myeditor/screen/home/HomeViewModel.java
@@ -8,23 +8,33 @@ import android.arch.lifecycle.LiveData;
 import android.arch.lifecycle.MutableLiveData;
 import android.content.Context;
 import android.databinding.ObservableField;
-import android.media.Image;
 import android.support.annotation.NonNull;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.view.ViewPager;
 import android.util.DisplayMetrics;
 import android.widget.ProgressBar;
+import framgia.com.myeditor.BuildConfig;
+import framgia.com.myeditor.data.model.Collection;
+import framgia.com.myeditor.data.model.Image;
+import framgia.com.myeditor.data.model.ImageRandom;
+import framgia.com.myeditor.data.repository.ImageRepository;
 import framgia.com.myeditor.screen.base.BaseViewModel;
+import framgia.com.myeditor.screen.home.adapter.CollectionAdapter;
+import framgia.com.myeditor.screen.home.adapter.NewAdapter;
+import framgia.com.myeditor.screen.home.adapter.RandomPagerAdapter;
 import framgia.com.myeditor.utils.common.DisplayUtils;
 import framgia.com.myeditor.utils.rx.BaseSchedulerProvider;
+import io.reactivex.Observable;
 import io.reactivex.disposables.CompositeDisposable;
 import io.reactivex.disposables.Disposable;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
 
 import static android.view.View.GONE;
+import static framgia.com.myeditor.screen.home.BindingHome.NUMBER_CONVERT;
+import static framgia.com.myeditor.screen.home.BindingHome.ONE_HUNDRED;
 
 /**
  * Created by CuD HniM on 18/10/03.
@@ -33,8 +43,6 @@ public class HomeViewModel extends BaseViewModel implements LifecycleOwner {
 
     private static final long TIME_DELAY = 10000;
     private static final long TIME_PRERIOD = 10000;
-    private static final int ONE_HUNDRED_NUMBER = 100;
-    private static final double NUMBER_CONVERT = 0.01;
     private Context mContext;
     private BaseSchedulerProvider mSchedulerProvider;
     private ImageRepository mRepository;
@@ -47,6 +55,7 @@ public class HomeViewModel extends BaseViewModel implements LifecycleOwner {
     private NewAdapter mNewAdapter;
     private List<Image> mImages;
     private ViewPager mPager;
+    private ImageRandom mImageRandom;
     private int mPageNumber = 1;
     private int mPageCollectionNumber = 1;
     private ProgressBar mProgressBar;
@@ -57,9 +66,9 @@ public class HomeViewModel extends BaseViewModel implements LifecycleOwner {
     private ObservableField<RandomPagerAdapter> mRandomObservableField = new ObservableField<>();
     private ObservableField<CollectionAdapter> mCollectionObservableField = new ObservableField<>();
 
-    HomeViewModel(Context context, ViewPager viewPager, ImageRepository repository) {
+    HomeViewModel(Context context, ViewPager viewPager, FragmentManager manager,
+            ImageRepository repository) {
         mContext = context;
-        initImageSize();
         mLifecycleRegistry = new LifecycleRegistry(this);
         mLifecycleRegistry.markState(Lifecycle.State.CREATED);
         mRepository = repository;
@@ -70,8 +79,9 @@ public class HomeViewModel extends BaseViewModel implements LifecycleOwner {
         mImages = new ArrayList<>();
         mCollections = new ArrayList<>();
         mNewList = new ArrayList<>();
-        mCollectionAdapter = new CollectionAdapter();
-        mNewAdapter = new NewAdapter();
+        mCollectionAdapter = new CollectionAdapter(context, manager);
+        mNewAdapter = new NewAdapter(mContext, manager);
+        initImageSize();
     }
 
     @NonNull
@@ -99,6 +109,19 @@ public class HomeViewModel extends BaseViewModel implements LifecycleOwner {
         mProgressBar = progressBar;
         mPageNumber++;
         getNews(mNewAdapter, mPageNumber);
+    }
+
+    private void getAllImage() {
+        Disposable disposable = mRepository.getAllImages()
+                .subscribeOn(mSchedulerProvider.io())
+                .observeOn(mSchedulerProvider.ui())
+                .subscribe(imageRandoms -> {
+                    for (ImageRandom image : imageRandoms) {
+                        insertImage(image);
+                    }
+                }, throwable -> DisplayUtils.makeToast(mContext, throwable.getMessage()), () -> {
+                });
+        mCompositeDisposable.add(disposable);
     }
 
     void OnCollectionLoadMore() {
@@ -159,6 +182,15 @@ public class HomeViewModel extends BaseViewModel implements LifecycleOwner {
                 .subscribe(images -> {
                     mNewList.addAll(images);
                     mNewData.setValue(mNewList);
+                    for (Image image : images) {
+                        ImageRandom imageRandom = new ImageRandom.Builder().mImageId(image.getId())
+                                .mRawImage(image.getUrls().getRaw())
+                                .mPath(image.getUrls().getRegular())
+                                .mUserName(image.getUser().getUsername())
+                                .mType(ImageType.REMOTE)
+                                .build();
+                        insertImage(imageRandom);
+                    }
                 }, throwable -> {
                     mNewData.setValue(null);
                     DisplayUtils.makeToast(mContext, throwable.getMessage());
@@ -196,12 +228,24 @@ public class HomeViewModel extends BaseViewModel implements LifecycleOwner {
         timer.scheduleAtFixedRate(new RandomTimer(), TIME_DELAY, TIME_PRERIOD);
     }
 
-    private void initImageSize() {
+    public void initImageSize() {
         DisplayMetrics displayMetrics = mContext.getResources().getDisplayMetrics();
         int width = displayMetrics.widthPixels;
         int height = displayMetrics.heightPixels;
-        float ratio = (ONE_HUNDRED_NUMBER * height) / width;
-        new BindingMain(width, (float) (ratio * NUMBER_CONVERT));
+        float ratio = (ONE_HUNDRED * mPager.getMeasuredHeight()) / width;
+        new BindingHome(width, height, (float) (ratio * NUMBER_CONVERT), ImageType.REMOTE);
+    }
+
+    void insertImage(ImageRandom imageRandom) {
+        Disposable disposable = Observable.create(emitter -> {
+            mRepository.insertImage(imageRandom);
+            emitter.onComplete();
+        }).subscribeOn(mSchedulerProvider.io()).observeOn(mSchedulerProvider.ui()).subscribe(o -> {
+
+        }, throwable -> DisplayUtils.makeToast(mContext, throwable.getMessage()), () -> {
+
+        });
+        mCompositeDisposable.add(disposable);
     }
 
     private class RandomTimer extends TimerTask {
---
+++ app/src/main/java/framgia/com/myeditor/screen/home/adapter/NewAdapter.java
@@ -0,0 +1,80 @@
+package framgia.com.myeditor.screen.home.adapter;
+
+import android.content.Context;
+import android.databinding.DataBindingUtil;
+import android.support.annotation.NonNull;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import framgia.com.myeditor.R;
+import framgia.com.myeditor.data.model.Image;
+import framgia.com.myeditor.data.model.ImageRandom;
+import framgia.com.myeditor.databinding.ItemNewBinding;
+import framgia.com.myeditor.screen.home.HandleItemClick;
+import java.util.ArrayList;
+import java.util.List;
+
+import static framgia.com.myeditor.screen.home.adapter.RandomPagerAdapter.NUMBER_ONE;
+import static framgia.com.myeditor.screen.home.adapter.RandomPagerAdapter.NUMBER_ZERO;
+
+/**
+ * Created by CuD HniM on 18/10/04.
+ */
+public class NewAdapter extends RecyclerView.Adapter<NewAdapter.NewHolder> {
+
+    private Context mContext;
+    private List<Image> mNewImages;
+    private FragmentManager mManager;
+
+    public NewAdapter(Context context, FragmentManager manager) {
+        mContext = context.getApplicationContext();
+        mManager = manager;
+        mNewImages = new ArrayList<>();
+    }
+
+    public void setNewImages(List<Image> newImages) {
+        mNewImages.addAll(newImages.subList(mNewImages.size(), newImages.size()));
+    }
+
+    @NonNull
+    @Override
+    public NewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        ItemNewBinding binding =
+                DataBindingUtil.inflate(LayoutInflater.from(viewGroup.getContext()),
+                        R.layout.item_new, viewGroup, false);
+        return new NewHolder(binding);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull NewHolder newHolder, int i) {
+        newHolder.binding(mContext, mManager, mNewImages.get(i));
+    }
+
+    @Override
+    public int getItemCount() {
+        return mNewImages == null ? NUMBER_ZERO : mNewImages.size();
+    }
+
+    static class NewHolder extends RecyclerView.ViewHolder {
+
+        private ItemNewBinding mBinding;
+
+        NewHolder(@NonNull ItemNewBinding itemView) {
+            super(itemView.getRoot());
+            mBinding = itemView;
+        }
+
+        void binding(Context context, FragmentManager manager, Image image) {
+            mBinding.setItem(new ImageRandom.Builder().mPath(image.getUrls().getRegular())
+                    .mLikeByUser(image.getLikedByUser() ? NUMBER_ONE : NUMBER_ZERO)
+                    .mUserName(image.getUser().getUsername())
+                    .mImageId(image.getId())
+                    .mRawImage(image.getUrls().getRaw())
+                    .mType(ImageType.REMOTE)
+                    .build());
+            mBinding.setListener(new HandleItemClick(context, manager));
+            mBinding.executePendingBindings();
+        }
+    }
+}
